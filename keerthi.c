3c5a33c23cebc7ba2136b3c762706ee068445235
keerthi1030 <50397323+keerthi1030@users.noreply.github.com>
2019-05-08 14:48:16 +0530

    Update keerthi.c

--- keerthi.c
+++ keerthi.c
@@ -1,53 +1,31 @@
-/*
- */
 
-#include <avr/io.h>
-#define SET_BIT(PORT,bit) PORT|= (1<<bit)
-#define CLR_BIT(PORT,bit) PORT&= ~(1<<bit)
-/*int main(void)
-{
-   //                                                                                                                                                                                                             DDRD = 0xff;
-    SET_BIT(DDRD,PD7); //Output pin
-    CLR_BIT(DDRD,PD3); //input that is switch
-    SET_BIT(PORTD,PD3); // as a pull up resistor that is we make it as high
-    while(1)
-    {
-         uint8_t pin_reset = 0x00;
-        pin_reset = PIND;
-        if(!(pin_reset & (1<<PD3))) // !1 = 0 since its pull up if we get 0 it will work
-        {
-            SET_BIT(PORTD,PD7);
-        }
-        else
-        {
-            CLR_BIT(PORTD,PD7);
-        }
-    }
-
-    return 0;
-}
-*/
-
-
-int main(void)
-{
-   //                                                                                                                                                                                                             DDRD = 0xff;
-    SET_BIT(DDRD,PD7); //Output pin
-    CLR_BIT(DDRD,PD3); //input that is switch
-    SET_BIT(PORTD,PD3); // as a pull up resistor that is we make it as high
-    while(1)
-    {
-         uint8_t pin_reset = 0x00;
-        pin_reset = PIND;
-        if(!(pin_reset & (1<<PD3))) // !1 = 0 since its pull up if we get 0 it will work
-        {
-            SET_BIT(PORTD,PD7);
-        }
-        else
-        {
-            CLR_BIT(PORTD,PD7);
-        }
-    }
-
-    return 0;
+#include <stdio.h> 
+#include <string.h> 
+  
+// A function to check if a string str is palindrome 
+void isPalindrome(char str[]) 
+{ 
+    // Start from leftmost and rightmost corners of str 
+    int l = 0; 
+    int h = strlen(str) - 1; 
+  
+    // Keep comparing characters while they are same 
+    while (h > l) 
+    { 
+        if (str[l++] != str[h--]) 
+        { 
+            printf("%s is Not Palindrome", str); 
+            return; 
+        } 
+    } 
+    printf("%s is palindrome", str); 
+} 
+  
+// Driver program to test above function 
+int main() 
+{ 
+    isPalindrome("abba"); 
+    isPalindrome("abbccbba"); 
+    isPalindrome("geeks"); 
+    return 0; 
 }
